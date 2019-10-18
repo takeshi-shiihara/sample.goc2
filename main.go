6502dda59184edc88f30f2bf25775025966c4440
takeshi-shiihara <shi4490@icloud.com>
2019-10-18 17:17:09 +0900

    Initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*.html")
+
+	data := "Hallo Go/Gin!"
+
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.HTML(200, "index.html", gin.H{"data": data})
+	})
+
+	router.Run()
+}
